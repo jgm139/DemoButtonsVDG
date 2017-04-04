6ce622c2f875e746205a02089bbcfacf04e8e336
Julia García Martínez <jgm139@alu.ua.es>
2017-04-04 17:52:23 +0200

    Pu** temporizador

--- DemoTAES/src/pages/buttons/buttons.ts
+++ DemoTAES/src/pages/buttons/buttons.ts
@@ -9,11 +9,13 @@ import { NavController } from 'ionic-angular';
 export class ButtonsPage {
 
   public message;
-  public inter;
-  public t;
+  //public inter;
+  //public t;
   constructor(public navCtrl: NavController, private alertController: AlertController) {
     this.message="";
-    clearInterval(this.inter);
+    //document.getElementById('timer').innerHTML = 0o3 + ":" + 0o0;
+    //this.startTimer();
+    //clearInterval(this.inter);
   }
 
   presentClick() {
@@ -40,7 +42,7 @@ export class ButtonsPage {
                 });
                 alert.present();
   }
-  clear(){
+  /*clear(){
         clearInterval(this.inter);
   }
   timeout(){
@@ -55,7 +57,32 @@ export class ButtonsPage {
             this.t++;
             document.getElementById("testdiv").innerHTML=this.t;
         },1000,"JavaScript");
+    }*/
+
+    checkSecond(sec) {
+      if (sec < 10 && sec >= 0) {
+        sec = "0" + sec;
+      }; // add zero in front of numbers < 10
+      if (sec < 0) {
+        sec = "59";
+      };
+      return sec;
     }
+
+    startTimer() {
+      var presentTime = document.getElementById('timer').innerHTML;
+      var timeArray = presentTime.split(/[:]+/);
+      var m = parseInt(timeArray[0]);
+      var s = this.checkSecond((parseInt(timeArray[1]) - 1));
+      if (s == 59) {
+        m = m - 1;
+      }
+      //if(m<0){alert('timer completed')}
+
+      document.getElementById('timer').innerHTML = m + ":" + s;
+      setTimeout(this.startTimer, 1000);
+    }
+
   helpOperator() {
     let alert = this.alertController.create({
                   title: "Asistente telefónico",
