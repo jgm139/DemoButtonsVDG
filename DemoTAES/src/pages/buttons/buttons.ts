ba09dfe91890d330d5eaf05ecff853d56d464b2a
Julia García Martínez <jgm139@alu.ua.es>
2017-04-05 23:18:03 +0200

    Falta ponerle el mapa de contacto

--- DemoTAES/src/pages/buttons/buttons.ts
+++ DemoTAES/src/pages/buttons/buttons.ts
@@ -2,10 +2,18 @@ import { Component, OnInit} from '@angular/core';
 import {AlertController} from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { SimpleTimer } from 'ng2-simple-timer';
+import { MapPage } from 'map/map';
+
 @Component({
   selector: 'page-buttons',
   templateUrl: 'buttons.html'
 })
+
+@Component({
+  templateUrl: 'map.html'
+})
+
+
 export class ButtonsPage implements OnInit {
 
   public message;
@@ -14,36 +22,42 @@ export class ButtonsPage implements OnInit {
   counter0 = 180;
 	timer0Id: string;
   sec="00";
-  //public inter;
-  //public t;
+
   constructor(public navCtrl: NavController, private alertController: AlertController, private st: SimpleTimer) {
     this.message="";
-    this.min=3;
-    this.secs=0;
-    //document.getElementById('timer').innerHTML = 0o3 + ":" + 0o0;
-    //this.startTimer();
-    //clearInterval(this.inter);
+    this.reset();
+    //this.goToContact();
+  }
+
+  goToContact() {
+    this.navCtrl.push(MapPage);
   }
 
 	ngOnInit() {
 		this.st.newTimer('1sec',1);
-		
 	}
 
+  reset() {
+    this.min=3;
+    this.secs=0;
+  }
+
   presentClick() {
-    this.message="Estamos en contacto";
-    this.alertPop("Informa", this.message, "OK"); 
+    this.stopTimer();
+    this.message="Alerta desactivada";
+    this.alertPop("Cancelar", this.message, "OK"); 
   }
 
   discussionClick() {
-    this.message="En 10 minutos, si no recibimos respuesta, mandamos una patrulla";
+    this.reset();
+    this.startTimer();
+    this.message="En 3 minutos, si no recibimos respuesta, mandamos una patrulla";
     this.alertPop("Cuidado", this.message, "OK");
   }
 
   dangerClick() {
     this.message="Una patrulla va hacia el domicilio/lugar"
     this.alertPop("¡Alerta!", this.message, "OK");
-    
   }
 
   alertPop(tittleText: string, subtitleText: string, buttonText: string) {
@@ -54,68 +68,45 @@ export class ButtonsPage implements OnInit {
                 });
                 alert.present();
   }
-  /*clear(){
-        clearInterval(this.inter);
-  }
-  timeout(){
-        this.clear();
-        setTimeout(function(){
-            document.getElementById("testdiv").innerHTML="Pasaron 2 segundos antes de que vieras esto.";
-        },2000,"JavaScript");
-  }
-  interval(){
-        this.t=1;
-        this.inter=setInterval(function(){
-            this.t++;
-            document.getElementById("testdiv").innerHTML=this.t;
-        },1000,"JavaScript");
-    }*/
 
-    checkSecond(sec) {
-      var s=""+sec;
-      if (sec < 10 && sec >= 0) {
-        s = "0" + sec;
-      }; // add zero in front of numbers < 10
-      if (sec < 0) {
-        s = "59";
-      };
-      return s;
-    }
+  checkSecond(sec) {
+    var s=""+sec;
+    if (sec < 10 && sec >= 0) {
+      s = "0" + sec;
+    }; // add zero in front of numbers < 10
+    if (sec < 0) {
+      s = "59";
+    };
+    return s;
+  }
 
-    startTimer() { 
-        
-        this.timer0Id = this.st.subscribe('1sec', e => this.timer0callback());
-        
+  startTimer() { 
+      this.timer0Id = this.st.subscribe('1sec', e => this.timer0callback());
+  }
+  stopTimer() {
+    this.st.unsubscribe(this.timer0Id);
+    this.min=3;
+    this.sec="00";
+  }
+
+  timer0callback() {
+    if(this.min!=0 || this.secs!=0){
+      this.sec=this.checkSecond(this.secs - 1);
+      this.secs=parseInt(this.sec);
+      if (this.secs == 59) {
+        this.min = this.min - 1;
+      }
     }
-    timer0callback() {
-      if(this.min!=0 || this.secs!=0){
-        this.sec=this.checkSecond(this.secs - 1);
-        this.secs=parseInt(this.sec);
-        if (this.secs == 59) {
-          this.min = this.min - 1;
-        }
-      }
-      else{
-        this.st.delTimer('1sec');
-        this.st.newTimer('1sec',1);
-        this.min=3;
-        alert('timer completed');
-      }
+    else{
+      this.st.delTimer('1sec');
+      this.st.newTimer('1sec',1);
+      this.min=3;
+      alert('Patrulla en camino');
+      this.goToContact();
+    }
 		  
-	  }
-    /*startTimer(){
-      var presentTime=document.getElementById('timer').innerHTML;
-      var timeArray = time.split(/[:]+/);
-      var m=parseInt(timeArray[0]);
-      var s = this.checkSecond(parseInt(timeArray[1]) - 1);
-      if (s == 59) {
-        m = m - 1;
-      }
-      //if(m<0){alert('timer completed')}
-      
-      document.getElementById('timer').innerHTML = m + ':' + s;
-      setTimeout(this.startTimer,1000);
-    }*/
+  }
+
   helpOperator() {
     let alert = this.alertController.create({
                   title: "Asistente telefónico",
